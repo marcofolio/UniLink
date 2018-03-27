daad0900eeb42278de12080fc87a20856f8cf22f
marcofolio <marcoroelkuiper@gmail.com>
2018-03-27 15:30:55 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # UniLink
+
+Source files for the article on [Marcofolio.net](http://www.marcofolio.net/) called "[*iOS Universal Links in Xamarin.Forms and ASP.NET Core*](https://marcofolio.net/ios-universal-links-xamarin-forms-asp-net-core/)" .
+
+Browse the code or read the article to get more information about how this works.
---
+++ UniLink/UniLink.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace UniLink
+{
+    public class App : Application
+    {
+        private Label _label;
+
+        public App()
+        {
+            _label = new Label()
+            {
+                HorizontalTextAlignment = TextAlignment.Center,
+                Text = "Welcome to Xamarin Forms!"
+            };
+
+            // The root page of your application
+            var content = new ContentPage
+            {
+                Title = "UniLink",
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = { _label }
+                }
+            };
+
+            MainPage = new NavigationPage(content);
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+
+		protected override void OnAppLinkRequestReceived(Uri uri)
+		{
+            _label.Text = "Xamarin Forms opened through App Link!";
+
+            base.OnAppLinkRequestReceived(uri);
+		}
+	}
+}
